a0d8c864eb772c8c847665e5f48676bca22b1c3d
Anton Krasov <anton.krasov@gmail.com>
2022-05-07 12:58:03 -0500

    protectedEndpoint API

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require("firebase-functions");
+const serviceAccount = require("./service-account.json");
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = initializeApp({
+    credential: cert(serviceAccount)
+})
+const auth = getAuth(app)
+
+exports.protectedEndpoint = functions.https.onRequest(async (request, response) => {
+    try {
+        const xApiToken = request.headers['x-api-token']
+        const decodedToken = await auth.verifyIdToken(xApiToken)
+        response.send({ decodedToken });
+    } catch (ex) {
+        response.status(403).json({
+            error: "'x-api-token' header is required!"
+        })
+    }
+});
